3bb2668dfcf5b9ecf0e51ce11fb499f49a0e34ca
MmCrunchyLotion <petersondane77@gmail.com>
2025-09-24 19:19:36 -0600

    Initial Move implementation

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -9,25 +9,32 @@ package chess;
 public class ChessMove {
 
     private ChessPosition startPosition;
-    private ChessPiece.PieceType pieceType;
-    private ChessGame.TeamColor team;
+    private ChessPosition endPosition;
+    private ChessPiece.PieceType promotionPiece;
+//    private ChessPiece.PieceType pieceType;
+//    private ChessGame.TeamColor team;
 
     public ChessMove(ChessPosition startPosition, ChessPosition endPosition,ChessPiece.PieceType promotionPiece) {
-        throw new RuntimeException("Not implemented");
+        setStartPosition(startPosition);
+        setEndPosition(endPosition);
+        setPromotionPiece(promotionPiece);
+//        throw new RuntimeException("Not implemented");
     }
 
     /**
      * @return ChessPosition of starting location
      */
     public ChessPosition getStartPosition() {
-        throw new RuntimeException("Not implemented");
+        return this.startPosition;
+//        throw new RuntimeException("Not implemented");
     }
 
     /**
      * @return ChessPosition of ending location
      */
     public ChessPosition getEndPosition() {
-        throw new RuntimeException("Not implemented");
+        return this.endPosition;
+//        throw new RuntimeException("Not implemented");
     }
 
     /**
@@ -37,26 +44,39 @@ public class ChessMove {
      * @return Type of piece to promote a pawn to, or null if no promotion
      */
     public ChessPiece.PieceType getPromotionPiece() {
-        throw new RuntimeException("Not implemented");
+        if (this.promotionPiece != null) {
+            return this.promotionPiece;
+        } else {
+            return null;
+        }
+//        throw new RuntimeException("Not implemented");
     }
 
     public void setStartPosition(ChessPosition startPosition) {
         this.startPosition = startPosition;
     }
 
-    public ChessPiece.PieceType getPieceType() {
-        return pieceType;
+    public void setEndPosition(ChessPosition endPosition) {
+        this.endPosition = endPosition;
     }
 
-    public void setPieceType(ChessPiece.PieceType pieceType) {
-        this.pieceType = pieceType;
+    public void setPromotionPiece(ChessPiece.PieceType promotionPiece) {
+        this.promotionPiece = promotionPiece;
     }
 
-    public ChessGame.TeamColor getTeam() {
-        return team;
-    }
+//    public ChessPiece.PieceType getPieceType() {
+//        return pieceType;
+//    }
+//
+//    public void setPieceType(ChessPiece.PieceType pieceType) {
+//        this.pieceType = pieceType;
+//    }
 
-    public void setTeam(ChessGame.TeamColor team) {
-        this.team = team;
-    }
+//    public ChessGame.TeamColor getTeam() {
+//        return team;
+//    }
+//
+//    public void setTeam(ChessGame.TeamColor team) {
+//        this.team = team;
+//    }
 }
