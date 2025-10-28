1752c5aec933bf71af4f9b232ab4d64f12dc1cd1
MmCrunchyLotion <petersondane77@gmail.com>
2025-10-27 21:19:33 -0600

    Added service, model, dao files. No code in these files.

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,13 +1,6 @@
 import chess.*;
-
-//public class Main {
-//    public static void main(String[] args) {
-//        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-//        System.out.println("♕ 240 Chess Server: " + piece);
-//    }
-//}
-
 import server.Server;
+//import service.ChessService;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,3 +10,27 @@ public class Main {
         System.out.println("♕ 240 Chess Server");
     }
 }
+
+//public class Main {
+//    public static void main(String[] args) {
+//        try {
+//            var port = 8080;
+//            if (args.length >= 1) {
+//                port = Integer.parseInt(args[0]);
+//            }
+//
+//            DataAccess dataAccess = new MemoryDataAccess();
+//            if (args.length >= 2 && args[1].equals("sql")) {
+//                dataAccess = new MySqlDataAccess();
+//            }
+//
+//            var service = new ChessService(DataAccess);
+//            var server = new Server(service).run(port);
+//            System.out.printf("Server started on port %d", port);
+//            return;
+//        } catch (Throwable ex) {
+//            System.out.printf("Unable to start server: %s%n", ex.getMessage());
+//        }
+//        System.out.println("♕ 240 Chess Server");
+//    }
+//}
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,16 +1,28 @@
 package server;
-
+import com.google.gson.Gson;
+import exception.ResponseException;
+import chess.ChessGame;
 import io.javalin.*;
+import io.javalin.http.Context;
+import service.ChessService;
 
 public class Server {
 
+//    private final ChessService service;
     private final Javalin javalin;
 
+//    public Server(ChessService service) {
+//        this.service = service;
     public Server() {
-        javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
-        // Register your endpoints and exception handlers here.
-
+        javalin = Javalin.create(config -> config.staticFiles.add("web"))
+                .post("/user", this::addUser)
+                .post("/session", this::login)
+                .delete("/session", this::logout)
+                .get("/game", this::listGames)
+                .post("/game", this::newGame)
+                .delete("/db", this::clear)
+                .exception(ResponseException.class, this::exceptionHandler);
     }
 
     public int run(int desiredPort) {
@@ -20,5 +32,36 @@ public class Server {
 
     public void stop() {
         javalin.stop();
+    }
+
+    private void exceptionHandler(ResponseException ex, Context ctx) {
+        ctx.status(ex.toHttpStatusCode());
+        ctx.result(ex.toJson());
+    }
+
+    private void addUser(Context ctx) {
+        User user = new Gson().fromJson(ctx.body(), User.class);
+        ctx.result(new Gson().toJson(user));
+    }
+
+    private void login(Context ctx) {
+
+    }
+
+    private void logout(Context ctx) {
+
+    }
+
+    private void listGames(Context ctx) {
+
+    }
+
+    private void newGame(Context ctx) {
+        ChessGame game = new Gson().fromJson(ctx.body(), ChessGame.class);
+
+    }
+
+    private void clear(Context ctx) {
+
     }
 }
