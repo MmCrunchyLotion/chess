90f9882cb511dbd18f33704557decc89b62da5d9
MmCrunchyLotion <petersondane77@gmail.com>
2026-02-27 20:31:05 -0700

    Create tables updated

--- server/src/main/java/dataaccess/MySqlDataAccess.java
+++ server/src/main/java/dataaccess/MySqlDataAccess.java
@@ -18,7 +18,6 @@ public class MySqlDataAccess {
                     Object param = params[i];
                     if (param instanceof String p) ps.setString(i + 1, p);
                     else if (param instanceof Integer p) ps.setInt(i + 1, p);
-//                    else if (param instanceof PetType p) ps.setString(i + 1, p.toString());
                     else if (param == null) ps.setNull(i + 1, NULL);
                 }
                 ps.executeUpdate();
@@ -37,17 +36,48 @@ public class MySqlDataAccess {
 
     private final String[] createStatements = {
             """
-            CREATE TABLE IF NOT EXISTS  pet (
-              `id` int NOT NULL AUTO_INCREMENT,
-              `name` varchar(256) NOT NULL,
-              `type` ENUM('CAT', 'DOG', 'FISH', 'FROG', 'ROCK') DEFAULT 'CAT',
-              `json` TEXT DEFAULT NULL,
-              PRIMARY KEY (`id`),
-              INDEX(type),
-              INDEX(name)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
-            """
+            CREATE TABLE IF NOT EXISTS user (
+                id int NOT NULL AUTO_INCREMENT,
+                username varchar(256) NOT NULL,
+                password varchar(256) NOT NULL,
+                email varchar(256) DEFAULT NULL,
+                PRIMARY KEY ('id')
+                INDEX(username)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+            CREATE TABLE IF NOT EXISTS game (
+                id int NOT NULL AUTO_INCREMENT,
+                whiteUsername int DEFAULT NULL,
+                blackUsername int DEFAULT NULL,
+                gameName varchar(256) NOT NULL,
+                PRIMARY KEY ('id'),
+                CONSTRAINT fk_white FOREIGN KEY (whiteUsername) REFERENCES user (id),
+                CONSTRAINT fk_black FOREIGN KEY (blackUsername) REFERENCES user (id)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+            CREATE TABLE IF NOT EXISTS auth (
+                id int NOT NULL AUTO_INCREMENT,
+                username varchar(256) NOT NULL,
+                token varchar(256) NOT NULL,
+                PRIMARY KEY ('id'),
+                INDEX(username),
+                INDEX(token)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+        """
     };
+
+//            """
+//            CREATE TABLE IF NOT EXISTS  pet (
+//              `id` int NOT NULL AUTO_INCREMENT,
+//              `name` varchar(256) NOT NULL,
+//              `type` ENUM('CAT', 'DOG', 'FISH', 'FROG', 'ROCK') DEFAULT 'CAT',
+//              `json` TEXT DEFAULT NULL,
+//              PRIMARY KEY (`id`),
+//              INDEX(type),
+//              INDEX(name)
+//            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+//            """
+//    };
 
     private void configureDatabase() throws ResponseException, DataAccessException {
         DatabaseManager.createDatabase();
