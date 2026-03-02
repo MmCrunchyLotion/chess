8c4ab2aa68ae114e6514be31f715ac5cd94fb366
MmCrunchyLotion <petersondane77@gmail.com>
2026-03-01 22:28:06 -0700

    Initial playing ui setup

--- client/src/main/java/ui/PlayingHandler.java
+++ client/src/main/java/ui/PlayingHandler.java
@@ -1,4 +1,54 @@
 package ui;
 
-public class PlayingHandler {
+import client.ServerFacade;
+
+public class PlayingHandler  extends Handler {
+
+    private ServerFacade server;
+    private UILoop.States state;
+
+    public PlayingHandler(ServerFacade facade) {
+        this.server = facade;
+        this.state = UILoop.States.PLAYING;
+    }
+
+    public void handle(String[] args) {
+        setArgs(args);
+        switch (arg0) {
+            case "help" -> help();
+            case "move" -> move(args);
+            case "quit" -> this.state = quit();
+            default -> System.out.println("Invalid command\n");
+        }
+        clearArgs();
+    }
+
+    private static void help() {
+        System.out.println("help - shows possible commands\n");
+        System.out.println("move <FROM> <TO> - move a piece\n");
+        System.out.println("quit - exit a chess game\n");
+        System.out.println("exit - exit the client\n");
+    }
+
+    private void move(String[] args) {
+        if (args.length != 3) {
+            System.out.println("Incorrect number of parameters\n");
+        } else {
+            // TODO: implement move logic when websocket is added
+            System.out.println("Moving from " + arg1 + " to " + arg2 + "\n");
+        }
+    }
+
+    public UILoop.States quit() {
+        System.out.println("Left the game\n");
+        return UILoop.States.LOGGED_IN;
+    }
+
+    public UILoop.States getState() {
+        return state;
+    }
+
+    public void setState(UILoop.States state) {
+        this.state = state;
+    }
 }
