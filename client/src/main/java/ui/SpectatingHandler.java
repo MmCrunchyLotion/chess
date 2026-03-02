f9f966a064e0006e87264ff0cbc1dafed2bd1e58
MmCrunchyLotion <petersondane77@gmail.com>
2026-03-01 22:28:42 -0700

    Initial spectating ui setup

--- client/src/main/java/ui/SpectatingHandler.java
+++ client/src/main/java/ui/SpectatingHandler.java
@@ -1,4 +1,43 @@
 package ui;
 
-public class SpectatingHandler {
+import client.ServerFacade;
+
+public class SpectatingHandler extends Handler {
+
+    private ServerFacade server;
+    private UILoop.States state;
+
+    public SpectatingHandler(ServerFacade facade) {
+        this.server = facade;
+        this.state = UILoop.States.SPECTATING;
+    }
+
+    public void handle(String[] args) {
+        setArgs(args);
+        switch (arg0) {
+            case "help" -> help();
+            case "quit" -> this.state = quit();
+            default -> System.out.println("Invalid command\n");
+        }
+        clearArgs();
+    }
+
+    private static void help() {
+        System.out.println("help - shows possible commands\n");
+        System.out.println("quit - exit a chess game\n");
+        System.out.println("exit - exit the client\n");
+    }
+
+    public UILoop.States quit() {
+        System.out.println("Stopped spectating\n");
+        return UILoop.States.LOGGED_IN;
+    }
+
+    public UILoop.States getState() {
+        return state;
+    }
+
+    public void setState(UILoop.States state) {
+        this.state = state;
+    }
 }
