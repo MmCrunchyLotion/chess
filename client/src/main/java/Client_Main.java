1675283c212a860565ed2adf6115dbf341b613b7
MmCrunchyLotion <petersondane77@gmail.com>
2026-04-16 15:29:15 -0600

    Bug fixes

--- client/src/main/java/Client_Main.java
+++ client/src/main/java/Client_Main.java
@@ -1,7 +1,7 @@
 import ui.UILoop;
 import exception.ResponseException;
 
-public class Main {
+public class Client_Main {
 
     public static void main(String[] args) throws ResponseException {
         System.out.println("♕ Welcome to 240 chess. Type 'Help' to get started. ♕\n");
